e294d17714da418e7cf40be010d1c2641dc1293f
Zhanibek <zhanibek.om@gmail.com>
2023-02-22 16:23:39 +0900

    start

--- src/Makemore.jl
+++ src/Makemore.jl
@@ -1,5 +1,16 @@
 module Makemore
 using Flux
-include("bigram.jl")
+
+function loaddata(filename)
+    lines = map(strip, filename |> open |> readlines)
+    uniquechars = Set{Char}()
+    for line in lines
+        for c in line
+            push!(uniquechars, c)
+        end
+    end
+
+    uniquechars_count = len(uniquechars)
+end
 
 end
