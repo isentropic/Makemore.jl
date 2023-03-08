092c4e3be4ae5a9d1dc08cffabc9507d9596e031
Zhanibek <zhanibek.om@gmail.com>
2023-03-08 11:29:10 +0900

    add basic dataset

--- src/Makemore.jl
+++ src/Makemore.jl
@@ -1,7 +1,33 @@
 module Makemore
 using Flux
+using Random
 
-function loaddata(filename)
+struct Dataset
+    words::Vector{String}
+    chars::Vector{Char}
+    max_word_length::Integer
+    stoi::Dict{Char,Integer}
+    itos::Dict{Integer,Char}
+end
+Base.length(dataset::Dataset) = length(dataset.words)
+
+function Dataset(words, chars, max_word_length)
+    stoi = Dict{Char,Integer}()
+    for (i, c) in enumerate(chars)
+        stoi[c] = i
+    end
+    itos = Dict{Integer,Char}()
+    for (k, v) in stoi
+        itos[v] = k
+    end
+
+    return Dataset(words, chars, max_word_length, stoi, itos)
+end
+
+encode(dataset::Dataset, word) = [dataset.stoi[c] for c in word]
+decode(dataset::Dataset, indices) = String([dataset.itos[ix] for ix in indices])
+
+function loaddatasets(filename, testsplit=0.1, toshuffle=true)
     lines = map(strip, filename |> open |> readlines)
     uniquechars = Set{Char}()
     for line in lines
@@ -10,7 +36,23 @@ function loaddata(filename)
         end
     end
 
-    uniquechars_count = len(uniquechars)
+    maxwordlength = 0
+    for word in lines
+        maxwordlength = max(maxwordlength, length(word))
+    end
+    uniquechars_count = length(uniquechars)
+
+    toshuffle && shuffle!(lines)
+
+    testcut = floor(Int, length(lines) * testsplit)
+    trainlines = lines[begin:end-testcut-1]
+    testlines = lines[end-testcut:end]
+
+    sortedchars = sort(collect(uniquechars))
+    train_dataset = Dataset(trainlines, sortedchars, maxwordlength)
+    test_dataset = Dataset(testlines, sortedchars, maxwordlength)
+
+    return train_dataset, test_dataset
 end
 
 end
